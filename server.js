5301f4a87e7c8dbe128429d10a45a402c3773da3
rage73 <raghavagrawal2000ra@gmail.com>
2021-08-23 00:57:23 +0530

    basic ui
    Signed-off-by: rage73 <raghavagrawal2000ra@gmail.com>

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 
 app.use('/', express.static(__dirname + '/public'));
 
+function decryptQueryParams(req,res,next) {
+    next();
+}
+
+function decodeQueryBase64(req,res,next) {
+    next();
+}
+
+app.get('/eval', decryptQueryParams, decodeQueryBase64, (req,res) => {
+    console.log(req.query);
+    res.send('!!result!!');
+})
+
 app.listen(4545, ()=>{
     console.log('server started at http://localhost:4545');
 })
